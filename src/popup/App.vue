caa90c8c9323ebe6a67d0435eaf62ead54be3dc0
amok6from7 <kwifi7@gmail.com>
2019-12-19 21:54:50 +0900

    getCookie

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,19 +1,40 @@
 <template>
   <div>
-    <p>Hello world!</p>
+    <p>Get Cookie Extension</p>
+    <button @click="getCookie">GET Cookie</button>
+    <ul>
+      <li v-for="(cookie, index) in cookies" :key="index">
+        <span>{{ index }}. {{ cookie.name }} : {{ cookie.value }}</span>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {
-  data () {
-    return {}
+  export default {
+    data () {
+      return {
+        cookies: null,
+        text: null
+      }
+    },
+    methods: {
+      getCookie() {
+        chrome.cookies.getAll({domain:'yahoo.co.jp'},(function(c) {
+          console.log(c)
+            this.cookies = c;
+        }.bind(this)))
+      }
+    }
   }
-}
 </script>
 
 <style lang="scss" scoped>
-p {
-  font-size: 20px;
-}
+  p {
+    font-size: 20px;
+    min-width: 300px;
+  }
+  ul {
+    list-style: none;
+  }
 </style>
